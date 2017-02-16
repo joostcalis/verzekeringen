7b80d922a72c6f5c25f8a4315853cf029c0c8bad
Joost Calis <joost_calis@hotmail.com>
2017-02-16 11:53:29 +0100

    removed nokogiri for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    require 'nokogiri'
-    require 'open-uri'
-    doc = Nokogiri::HTML(open("http://www.telegraaf.nl"))
-    puts doc.css("//a")
     @insurers = Insurer.all
   end
 
