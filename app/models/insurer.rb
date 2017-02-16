8cb91b513d2c50f8983d748f078c01c347c98dca
Joost Calis <joost_calis@hotmail.com>
2017-02-16 11:54:06 +0100

    relations for insurer and included publishable module

--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -1,5 +1,9 @@
 class Insurer < ActiveRecord::Base
+  include Publishable
+  has_many :answers
+
   validates :name, presence: true, uniqueness: true
   validates :affiliate_link, presence: true, uniqueness: true
   validates :meta_page_title, presence: true, uniqueness: true
+
 end
